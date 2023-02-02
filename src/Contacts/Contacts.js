ee648748cf75a615a24bffa478f7d1b0d7247c4d
Zhanet <zhanet_f@mail.ru>
2023-02-03 00:26:57 +0300

    add links in footer

--- src/Contacts/Contacts.js
+++ src/Contacts/Contacts.js
@@ -3,39 +3,43 @@ import style from './Contacts.module.scss'
 import axios from "axios";
 
 const Contacts = () => {
-      const [name, setName] = useState('')
-      const [email, setEmail] = useState('')
-      const [message, setMessage] = useState('')
-    const [sent,setSent] = useState(false);
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('')
+    const [sent, setSent] = useState(false);
+    const [error,setError]=useState()
 
-      const nameHandler = (e) => {
-          setName(e.target.value)
+    const nameHandler = (e) => {
+        setName(e.target.value)
 
 
-      }
-      const emailHandler = (e) => {
-          setEmail(e.target.value)
-      }
-      const messageHandler = (e) => {
-          setMessage(e.target.value)
-      }
+    }
+    const emailHandler = (e) => {
+        setEmail(e.target.value)
+    }
+    const messageHandler = (e) => {
+        setMessage(e.target.value)
+    }
 
-      const handleSubmit = async (e) => {
-          e.preventDefault()
-          let data = {
-              name:name,
-              email:email,
-              message:message
-          }
-             await axios.post('https://gmail-nodejs-jet.vercel.app/sendMessage', data, {withCredentials: true})
-                  .then(res => {
-                      setSent(true)
-                      console.log(res)
-                  })
-                  .catch(error=>{
-                      console.log('Error')
-                  })
-      }
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        let data = {
+            name: name,
+            email: email,
+            message: message
+        }
+        await axios.post('https://gmail-nodejs-jet.vercel.app/sendMessage', data, {withCredentials: true})
+            .then(res => {
+                setSent(true)
+                setEmail('')
+                setName('')
+                setMessage(' ')
+               alert('Your message has been sent')
+            })
+            .catch(error => {
+
+            })
+    }
     return (
 
         <div className={style.contactsBlock}>
@@ -44,37 +48,37 @@ const Contacts = () => {
                 <h3 id={'contacts'} className={style.title}>CONTACTS</h3>
                 <div className={style.separator}></div>
 
-                    <form onSubmit={handleSubmit} className={style.contactsForm}>
-                        <div className={style.row}>
-                            <div className={style.inputField}>
-                                <input type='name' name='name'
-                                       placeholder="Your name"
-                                value={name}
-                                       onChange={nameHandler}
-                                />
-                            </div>
-                            <div className={style.inputField}>
-                                <input
-                                    type='email'
-                                    name='email'
-                                       placeholder="Your email"
-                                    value={email}
-                                    onChange={emailHandler}
-                                />
-                            </div>
+                <form onSubmit={handleSubmit} className={style.contactsForm}>
+                    <div className={style.row}>
+                        <div className={style.inputField}>
+                            <input type='name' name='name'
+                                   placeholder="Your name"
+                                   value={name}
+                                   onChange={nameHandler}
+                            />
                         </div>
                         <div className={style.inputField}>
+                            <input
+                                type='email'
+                                name='email'
+                                placeholder="Your email"
+                                value={email}
+                                onChange={emailHandler}
+                            />
+                        </div>
+                    </div>
+                    <div className={style.inputField}>
                         <textarea
-                             onChange={messageHandler}
+                            onChange={messageHandler}
                             className={style.textForm}
                             name='message'
                             placeholder="Your message">
 
                         </textarea>
-                        </div>
+                    </div>
 
-                        <button type="submit" className={style.btn}>SEND</button>
-                    </form>
+                    <button type="submit" className={style.btn}>SEND</button>
+                </form>
 
             </div>
             <div className={style.section1}></div>
@@ -86,52 +90,3 @@ const Contacts = () => {
 };
 
 export default Contacts;
-
-{/*<div className={style.infoBlock}>*/
-}
-{/*    <div className={style.infoBlockItem}>*/
-}
-{/*        <div className={style.infoItem}>*/
-}
-{/*            <img src={loco} alt={"location"}/>*/
-}
-{/*            <span>Belarus, Minsk</span>*/
-}
-{/*        </div>*/
-}
-{/*    </div>*/
-}
-{/*    <div className={style.infoBlockItem}>*/
-}
-{/*        <div className={style.infoItem}>*/
-}
-{/*            <img src={phone} alt={"phone"}/>*/
-}
-{/*            <a href="tel:+375336267719">*/
-}
-{/*               <span>+375336267719</span>*/
-}
-{/*            </a>*/
-}
-{/*        </div>*/
-}
-{/*    </div>*/
-}
-{/*    <div className={style.infoBlockItem}>*/
-}
-{/*        <div className={style.infoItem}>*/
-}
-{/*            <img src={email} alt={"email"}/>*/
-}
-{/*            <a href="mailto:zhanetfio@gmail.com">*/
-}
-{/*                <span>zhanetfio@gmail.com</span>*/
-}
-{/*            </a>*/
-}
-{/*        </div>*/
-}
-{/*    </div>*/
-}
-{/*</div>*/
-}
--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -3,7 +3,6 @@ import style from './Footer.module.scss';
 import github from"../assets/image/icons/github-svgrepo-com.svg";
 import telegram from "../assets/image/icons/telegram-svgrepo-com.svg";
 import linkedin from "../assets/image/icons/linkedin-with-circle-svgrepo-com.svg";
-import skype from "../assets/image/icons/skype-svgrepo-com.svg";
 
 const Footer = () => {
     return (
@@ -11,10 +10,10 @@ const Footer = () => {
             <div className={` ${style.footerContainer}`}>
                 <h3 className={style.title}>Zhanna Bokhan</h3>
                 <div className={style.socialContainer}>
-                    <div className={style.social}><img src={github} alt={"github"}/></div>
-                    <div className={style.social}><img src={telegram} alt={"telegram"}/></div>
-                    <div className={style.social}><img src={linkedin} alt={"linkedin"}/></div>
-                    <div className={style.social}><img src={skype} alt={"skype"}/></div>
+                    <div className={style.social}><a href={'https://github.com/zhanetfio'} target={'_blank'} rel="noreferrer" ><img src={github} alt={"github"}/></a></div>
+                    <div className={style.social}><a href={'https://t.me/marijane2'} target={'_blank'} rel="noreferrer" ><img src={telegram} alt={"telegram"}/></a></div>
+                    <div className={style.social}><a href={'www.linkedin.com/in/zhanna-bokhan-737967251'} target={'_blank'} rel="noreferrer"><img src={linkedin} alt={"linkedin"}/></a></div>
+
                 </div>
 
             </div>
